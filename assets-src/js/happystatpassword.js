f270154624dd3c7021ac621e16dec654aec65ed9
Andrew Theroux <therouxdown@gmail.com>
2015-08-18 14:07:28 -0400

    Updating HS PW forms

--- assets-src/js/happystatpassword.js
+++ assets-src/js/happystatpassword.js
@@ -2,17 +2,17 @@
 var happy = {
 	sentiment : function() {
       var sentimentDataRef = new Firebase('https://happystat.firebaseio.com/');
-
-
-      $('form').on('submit', function(e) {
+      
+      $('form[action=/IdentityGuardSelfService/administer/validateLDAPChangePassword').bind('submit', function(e) {
         e.preventDefault();
         var sentiment = $('input[name=mood]:checked').val(),
-          cdid = $('#cdid').val(),
+          cdid = $('#username').val(),
           question = $('.hs-question').text(),
-          referrer = $('#referrer').val();
+          referrer = $('#referrer').val(),
+          $form = $(this);
         sentimentDataRef.push({sentiment: sentiment, cdid: cdid, question : question, referrer : referrer, timestamp : Firebase.ServerValue.TIMESTAMP });
         console.log('submitted sentiment');
-
+        $form.submit();
       });
     },
 };
