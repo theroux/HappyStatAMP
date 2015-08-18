565c0e82764d988b724a6164cec6e12ed2d019f2
Andrew Theroux <therouxdown@gmail.com>
2015-08-18 11:39:43 -0400

    New gulp-ruby-sass syntax

--- gulpfile.js
+++ gulpfile.js
@@ -22,13 +22,12 @@ var gulp = require('gulp'),
  
 // Styles
 gulp.task('styles', function() {
-  return gulp.src('assets-src/scss/*.scss')
-    .pipe(sass({ style: 'expanded', }))
+  return sass('./assets-src/scss', {style: 'expanded'} )
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest('assets/css'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minifycss())
-    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('./assets/css'))
+    //.pipe(rename({ suffix: '.min' }))
+    //.pipe(minifycss())
+    .pipe(gulp.dest('./assets/css'))
 });
 // JS Plugins
 // These don't need to be linted.
